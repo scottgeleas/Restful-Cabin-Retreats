bd175b9cfea7fc0dc182596ae4642fa6a7d12dad
Scott Geleas <scottgeleas97@gmail.com>
2021-06-15 19:59:21 -0400

    started api routes

--- controllers/api/cabinRoutes.js
+++ controllers/api/cabinRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { Location } = require('../../models');
+const { Cabin } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    let locations = await Location.findAll();
-    let locationData = locations.map(item => {
+    let cabins = await Cabin.findAll();
+    let cabinData = cabins.map(item => {
       return item.get({ plain: true })
     })
-    res.status(200).json(locationData);
+    res.status(200).json(cabinData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -15,10 +15,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const newLocation = await Location.create({
+    const newCabin = await Cabin.create({
       ...req.body,
     });
-    res.status(200).json(newLocation);
+    res.status(200).json(newCabin);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -26,18 +26,18 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const locationData = await Location.destroy({
+    const cabinData = await Cabin.destroy({
       where: {
         id: req.params.id
       },
     });
 
-    if (!locationData) {
-      res.status(404).json({ message: 'No locations found with this id!' });
+    if (!cabinData) {
+      res.status(404).json({ message: 'No cabins found with this id!' });
       return;
     }
 
-    res.status(200).json(locationData);
+    res.status(200).json(cabinData);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/api/reservationRoutes.js
+++ controllers/api/reservationRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
-const { Location } = require('../../models');
+const { Reservation } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    let locations = await Location.findAll();
-    let locationData = locations.map(item => {
+    let reservations = await Reservation.findAll();
+    let reservationData = reservations.map(item => {
       return item.get({ plain: true })
     })
-    res.status(200).json(locationData);
+    res.status(200).json(reservationData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -15,10 +15,10 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const newLocation = await Location.create({
+    const newReservation = await Reservation.create({
       ...req.body,
     });
-    res.status(200).json(newLocation);
+    res.status(200).json(newReservation);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -26,18 +26,18 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const locationData = await Location.destroy({
+    const reservationData = await Reservation.destroy({
       where: {
         id: req.params.id
       },
     });
 
-    if (!locationData) {
-      res.status(404).json({ message: 'No locations found with this id!' });
+    if (!reservationData) {
+      res.status(404).json({ message: 'No reservations found with this id!' });
       return;
     }
 
-    res.status(200).json(locationData);
+    res.status(200).json(reservationData);
   } catch (err) {
     res.status(500).json(err);
   }
